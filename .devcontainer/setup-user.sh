3b3ad16d8e82a0bc867597d68835ca0dbac81743
Julio Arruda <julio.arruda@outlook.com>
2022-08-15 21:49:54 -0300

    test suername

--- .devcontainer/setup-user.sh
+++ .devcontainer/setup-user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-USERNAME=${1:-codespace}
+USERNAME=${1:-arruda}
 SECURE_PATH_BASE=${2:-$PATH}
 
 echo "Defaults secure_path=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin:${SECURE_PATH_BASE}\"" >> /etc/sudoers.d/securepath
